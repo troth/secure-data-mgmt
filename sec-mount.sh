8dcc4b114de0d9f3e6bff23761ac2b8c934a3e89
Theodore A. Roth <troth@openavr.org>
2023-07-15 10:28:59 -0600

    Exit on error
    Signed-off-by: Theodore A. Roth <troth@openavr.org>

--- sec-mount.sh
+++ sec-mount.sh
@@ -9,6 +9,6 @@ set -x
 
 mkdir -p ${MNT_PT}
 
-losetup ${LOOP} ${IMG}
-cryptsetup luksOpen ${LOOP} ${IMG}
-mount /dev/mapper/${IMG} ${MNT_PT}
+losetup ${LOOP} ${IMG}  || exit 1
+cryptsetup luksOpen ${LOOP} ${IMG}  || exit 1
+mount /dev/mapper/${IMG} ${MNT_PT}  || exit 1
