b7f9a7da9d3a038a9b053329de84ed44b0a2bcb9
Theodore A. Roth <troth@openavr.org>
2024-09-28 08:04:46 -0600

    Improve error handling when creating secure image
    Signed-off-by: Theodore A. Roth <troth@openavr.org>

--- sec-create.sh
+++ sec-create.sh
@@ -7,11 +7,21 @@
 
 set -x
 
-dd if=/dev/urandom of=${IMG} bs=1M count=${SIZE_MB}
+if [ -f "${IMG}" ]
+then
+    # Don't wnat to clobber an existing file.
+    echo "ERROR: Image file already exists: ${IMG}"
+    exit 1
+fi
+
+dd if=/dev/urandom of=${IMG} bs=1M count=${SIZE_MB} || exit 1
+
 LOOP=$(losetup --find --show ${IMG})
-cryptsetup luksFormat -c "aes-xts-essiv:sha256" -s 512 -y ${LOOP}
-cryptsetup luksOpen ${LOOP} ${IMG}
-mkfs.ext4 /dev/mapper/${IMG}
+test -z "${LOOP}" && exit 1
+
+cryptsetup luksFormat -c "aes-xts-essiv:sha256" -s 512 -y ${LOOP} || exit 1
+cryptsetup luksOpen ${LOOP} ${IMG} || exit 1
+mkfs.ext4 /dev/mapper/${IMG} || exit 1
 sync
 cryptsetup luksClose ${IMG}
 sync
