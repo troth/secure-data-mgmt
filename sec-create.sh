094e13eae3e8e698885f11dff23e57dda3b92a0b
Theodore A. Roth <troth@openavr.org>
2024-09-29 15:55:48 -0600

    sec-create.sh: Improve error handling
    Cleanup on errors so as to not leave the sec-data image in an ugly
    state.
    
    Signed-off-by: Theodore A. Roth <troth@openavr.org>

--- sec-create.sh
+++ sec-create.sh
@@ -5,8 +5,6 @@
 
 . ./sec-data-cfg.sh
 
-set -x
-
 if [ -f "${IMG}" ]
 then
     # Don't wnat to clobber an existing file.
@@ -14,15 +12,19 @@ then
     exit 1
 fi
 
+set -x
+
 dd if=/dev/urandom of=${IMG} bs=1M count=${SIZE_MB} || exit 1
 
 LOOP=$(losetup --find --show ${IMG})
-test -z "${LOOP}" && exit 1
-
-cryptsetup luksFormat -c "aes-xts-essiv:sha256" -s 512 -y ${LOOP} || exit 1
-cryptsetup luksOpen ${LOOP} ${IMG} || exit 1
-mkfs.ext4 /dev/mapper/${IMG} || exit 1
-sync
-cryptsetup luksClose ${IMG}
-sync
-losetup -d ${LOOP}
+test -n "${LOOP}" && {
+    cryptsetup luksFormat -c "aes-xts-essiv:sha256" -s 512 -y ${LOOP} && {
+        cryptsetup luksOpen ${LOOP} ${IMG} && {
+            mkfs.ext4 /dev/mapper/${IMG}
+            sync
+            cryptsetup luksClose ${IMG}
+            sync
+        }
+    }
+    losetup -d ${LOOP}
+}
