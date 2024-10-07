7a1bcded163a00e2625ec274c5d7804af88188e4
Theodore A. Roth <troth@openavr.org>
2024-10-07 06:24:30 -0600

    Change cryptsetup format command
    Use the currently recommended cipher when formating a luks image.
    
    Signed-off-by: Theodore A. Roth <troth@openavr.org>

--- sec-create.sh
+++ sec-create.sh
@@ -12,13 +12,24 @@ then
     exit 1
 fi
 
+CIPHER=(
+    # --cipher
+
+    # This the default and recommended cipher.
+    # aes-xts-plain64
+
+    # This is what I have used in the past (used to be recommended).
+    # aes-xts-essiv:sha256
+    # -s 512
+)
+
 set -x
 
 dd if=/dev/urandom of=${IMG} bs=1M count=${SIZE_MB} || exit 1
 
 LOOP=$(losetup --find --show ${IMG})
 test -n "${LOOP}" && {
-    cryptsetup luksFormat -c "aes-xts-essiv:sha256" -s 512 -y ${LOOP} && {
+    cryptsetup --type luks2 luksFormat "${CIPHER[@]}" -y ${LOOP} && {
         cryptsetup luksOpen ${LOOP} ${IMG} && {
             mkfs.ext4 /dev/mapper/${IMG}
             sync
