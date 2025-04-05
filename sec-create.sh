9d8cd0b6ef0301cc24309827eb925e810dd5a5ba
Theodore A. Roth <troth@openavr.org>
2025-04-05 08:04:58 -0600

    Remove cipher specification
    Not using it and don't need to specify it since the defaults are
    sufficient.
    
    Signed-off-by: Theodore A. Roth <troth@openavr.org>

--- sec-create.sh
+++ sec-create.sh
@@ -12,24 +12,13 @@ then
     exit 1
 fi
 
-CIPHER=(
-    # --cipher
-
-    # This the default and recommended cipher.
-    # aes-xts-plain64
-
-    # This is what I have used in the past (used to be recommended).
-    # aes-xts-essiv:sha256
-    # -s 512
-)
-
 set -x
 
 dd if=/dev/urandom of=${IMG} bs=1M count=${SIZE_MB} || exit 1
 
 LOOP=$(losetup --find --show ${IMG})
 test -n "${LOOP}" && {
-    cryptsetup luksFormat --type luks2 --pbkdf argon2id "${CIPHER[@]}" -y ${LOOP} && {
+    cryptsetup luksFormat --type luks2 --pbkdf argon2id -y ${LOOP} && {
         cryptsetup luksOpen ${LOOP} ${IMG} && {
             mkfs.ext4 /dev/mapper/${IMG}
             sync
